0d29a0797cc581a9f09693f0f76f4404d5b16a3a
dishu001 <2020eeb1167@iitrpr.ac.in>
2024-02-15 22:23:29 +0530

    some changes

--- src/store/epic/rootEpic.ts
+++
@@ -1,4 +0,0 @@
-import { combineEpics } from "redux-observable";
-import { getAuthEpic } from "./auth";
-
-export const rootEpic = combineEpics(getAuthEpic);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/rootReducers";
-import { createEpicMiddleware } from "redux-observable";
-import { rootEpic } from "./epic/rootEpic";
 import { persistStore, persistReducer } from "redux-persist";
 import storageSession from "reduxjs-toolkit-persist/lib/storage/session";
 import thunk from "redux-thunk";
@@ -14,16 +12,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const epicMiddleware = createEpicMiddleware();
-
-
 
 const AppReduxStore = configureStore({
     reducer: persistedReducer,
-    middleware: [epicMiddleware, thunk,],
+    middleware: [thunk],
 });
 
-epicMiddleware.run(rootEpic);
 
 const persistor = persistStore(AppReduxStore);
 export { AppReduxStore, persistor };
