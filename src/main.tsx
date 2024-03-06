6e2f539e6025dbbc3611e13cd9ad493957a05ffa
dishu001 <2020eeb1167@iitrpr.ac.in>
2024-03-06 23:04:33 +0530

    added basename

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={AppReduxStore}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/CVPR_LAB/">
           <App />
         </BrowserRouter>
       </PersistGate>
