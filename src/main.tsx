bda762e019face4c4f6a7cfc712b85086550301d
dishu001 <2020eeb1167@iitrpr.ac.in>
2024-03-07 08:44:18 +0530

    dafsgh

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={AppReduxStore}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter basename="/CVPR_LAB/">
+        <HashRouter>
           <App />
         </HashRouter>
       </PersistGate>
